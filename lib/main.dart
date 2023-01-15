83811d1353584833355220dee8f332370cde21cc
hasan yilmaz <83864802+hasanyilmaz61@users.noreply.github.com>
2023-01-16 00:12:45 +0100

    flutter Liestview

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            title: Text("litview"),
+            title: Text("Listview"),
             centerTitle: true,
           ),
           body: lieswiew(),
@@ -29,138 +29,159 @@ class lieswiew extends StatefulWidget {
 }
 
 class _lieswiewState extends State<lieswiew> {
+  var liste = [
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+    Container(
+      height: 80,
+      color: Colors.cyan[50],
+      child: ListTile(
+        trailing: Icon(Icons.navigate_next),
+        subtitle: Text("corbalar icin tiklayiniz..."),
+        title: Text("corbalar"),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
+        ),
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("Mercimek corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("Domates corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("ezo gelin corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("mantar corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("yogurt corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("Köfteli Havuçlu Yoğurt corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("Tavuk Corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("Arpa Sehriyeli Corba"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("Pirasa Corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-        Container(
-          height: 80,
-          color: Colors.cyan[50],
-          child: NewWidgetlisview(
-            text: Text("Brokoli corbasi"),
-            img: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.yemek.com/mnresize/940/940/uploads/2014/06/mercimek-corbasi-yemekcom.jpg"),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class NewWidgetlisview extends StatelessWidget {
-  final Text text;
-  final CircleAvatar img;
-
-  NewWidgetlisview({required this.text, required this.img});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      trailing: Icon(Icons.navigate_next),
-      subtitle: Text("corbalar icin tiklayiniz..."),
-      title: text,
-      leading: img,
+    return ListView.builder(
+      itemCount: liste.length,
+      itemBuilder: (context, index) {
+        return liste[index];
+      },
     );
   }
 }
